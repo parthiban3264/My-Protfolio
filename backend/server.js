bebbf98686e1f323b6fa4b8405321fdb655535c5
parthiban3264 <parthiban.tcr.2004@gmail.com>
2025-06-21 12:32:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
