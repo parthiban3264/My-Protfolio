b77cc11ba48d49ea44c04ba48d6ba62928037968
parthiban3264 <parthiban.tcr.2004@gmail.com>
2025-06-21 12:42:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,6 @@ app.post('/send', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
