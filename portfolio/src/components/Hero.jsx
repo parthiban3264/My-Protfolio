969ba985eb52f9b0e2f816848ef6591df2f729dd
parthiban <parthibanvishva2004@gmail.com>
2025-07-12 11:54:24 +0530

    hero

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -25,9 +25,13 @@ const Hero = () => {
         <button
           className='bg-gradient-to-r from-green-400 to-blue-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</button>
-        <button
-          className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</button>
+        <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+          <button
+            className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
+            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
+            Resume
+          </button>
+        </a>
       </div>
 
     </div>
